0649613171f77995de367aa05322fb77205dc3b4
Idoko Augustine <linkwithaugustine@gmail.com>
2025-06-08 15:10:00 -0700

    last commit

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -36,8 +36,8 @@ Is this temperature in Celsius or Fahrenheit? (C/F): C
 
 
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 # 🔄 Convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
